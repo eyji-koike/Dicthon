27487b2b7790db124b3dea86b6692a1f1fbb8bb4
EyjiCuff <eyji.koike@gmail.com>
2022-08-29 16:07:50 -0300

    Extract parts of speech and definitions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pprint
+
+
 def get_dict_entry(word):
     import requests
     modelLink = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
@@ -5,8 +8,11 @@ def get_dict_entry(word):
     return entry.json()
 
 
-
 if __name__ == '__main__':
-    hello = get_dict_entry('hello')
+    hello = get_dict_entry('pen')
+    hello_meanings = [y['meanings'] for y in hello[:]]
+    parts_of_speech = [x[0]['partOfSpeech'] for x in hello_meanings[:]]
+    definitions = [x[0]['definitions'] for x in hello_meanings[:]]
     print(len(hello))
-    print([x['partOfSpeech'] for x in hello[0]['meanings'][:]])
+    print(hello_meanings)
+    pprint.pprint(definitions)
