c5a3aa65cba95e887e98ef4387006afa71163a28
EyjiCuff <eyji.koike@gmail.com>
2022-08-29 15:39:23 -0300

    Created a sorting method

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_dict_entry(word):
+    import requests
+    modelLink = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
+    entry = requests.get(modelLink)
+    return entry.json()
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    hello = get_dict_entry('hello')
+    print(len(hello))
+    print([x['partOfSpeech'] for x in hello[0]['meanings'][:]])
