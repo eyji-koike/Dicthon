2348c5c1ae3a9f0c1978a796290463ba7c2feffa
EyjiCuff <eyji.koike@gmail.com>
2022-09-06 15:24:28 -0300

    added tests, prettified import

--- Dicthon/dictionary_api_entry.py
+++ Dicthon/dictionary_api_entry.py
@@ -1,21 +1,21 @@
 class Dictionary_API_Entry:
-    '''
+    """
     Instantiate a Dictionary API entry. https://dictionaryapi.dev/
     This instance will query the entry on the API and enables you
     to access the following:
-        ✔︎ Audio Pronunciation Links
-        ✔︎ List of Antonyms
-        ✔︎ List of Synonyms
-        ✔︎ List of Word Classes
-        ✔︎ List of Definitions
-        ✔︎ List of Phonetic Text Representations
-        ✔︎ Raw JSON entry
+        ✔︎ List of Audio Pronunciation Links\n
+        ✔︎ List of Antonyms\n
+        ✔︎ List of Synonyms\n
+        ✔︎ List of Word Classes\n
+        ✔︎ List of Definitions\n
+        ✔︎ List of Definitions with Example\n
+        ✔︎ List of Phonetic Text Representations\n
+        ✔︎ Raw JSON entry\n
+    :param word: The word to search.
+    :type word: str.
+    """
 
-
-
-    '''
-
-    def __init__(self, word):
+    def __init__(self, word: str):
         self.audio_links = []
         self.antonyms = []
         self.synonyms = []
@@ -29,6 +29,13 @@ class Dictionary_API_Entry:
         self.raw_entry = self.get_dict_entry()
 
     def get_dict_entry(self):
+        """
+        Based on the word defined in the initialization,
+        do a GET request in the API.
+
+        :return: the API answer.
+        :rtype: list
+        """
         import requests
         model_link = f'https://api.dictionaryapi.dev/api/v2/entries/en/{self.word}'
         entry = requests.get(model_link)
@@ -36,51 +43,128 @@ class Dictionary_API_Entry:
         return self.raw_entry
 
     def get_all_meanings(self):
-        for meanings in self.raw_entry[:]:
-            if 'meanings' in meanings.keys():
-                self.meanings_list.append(meanings['meanings'])
+        """
+        A list with lists of meanings
+
+        :return: list with lists of meanings.
+        :rtype: list
+        """
+        for item in self.raw_entry[:]:
+            if 'meanings' in item.keys():
+                self.meanings_list.append(item['meanings'])
         return self.meanings_list
 
     def get_all_parts_of_speech(self):
-        self.parts_of_speech = [partsOfSpeech[0]['partOfSpeech'] for partsOfSpeech in self.meanings_list[:]]
+        """
+        A list with all classes the word has.
+
+        :return: list word classes.
+        :rtype: list
+        """
+        for meanings in self.meanings_list[:]:
+            for meaning in meanings[:]:
+                if 'partOfSpeech' in meaning.keys() and meaning['partOfSpeech'] not in self.parts_of_speech:
+                    self.parts_of_speech.append(meaning['partOfSpeech'])
         return self.parts_of_speech
 
     def get_all_definitions(self):
-        for definitions in self.meanings_list[:]:
-            for definition in definitions[0]['definitions']:
-                if 'definition' in definition.keys():
-                    self.definitions.append(definition['definition'])
+        """
+        A list with all the word definitions
+
+        :return: list with definitions.
+        :rtype: list
+        """
+        for parts_of_speech in self.meanings_list[:]:
+            for part_of_speech in parts_of_speech[:]:
+                if ('definitions' in part_of_speech.keys()) and type(part_of_speech['definitions']) == list:
+                    for definition in part_of_speech['definitions']:
+                        if 'definition' in definition.keys() and definition['definition'] != []:
+                            self.definitions.append(definition['definition'])
         return self.definitions
 
     def get_all_definitions_with_example(self):
-        for definitions in self.meanings_list[:]:
-            for definition in definitions[0]['definitions']:
-                if 'example' in definition.keys():
-                    self.definitions_with_example.append({'definition': definition['definition'],
-                                                          'example': definition['example']})
+        """
+        A list with all the word definitions which have an example.
+
+        :return: list definition and example.
+        :rtype: list
+        """
+        for parts_of_speech in self.meanings_list[:]:
+            for part_of_speech in parts_of_speech[:]:
+                if ('definitions' in part_of_speech.keys()) and type(part_of_speech['definitions']) == list:
+                    for definition in part_of_speech['definitions']:
+                        if 'example' in definition.keys() and definition['example'] != []:
+                            self.definitions_with_example.append({'definition': definition['definition'], 'example': definition['example']})
         return self.definitions_with_example
 
     def get_all_phonetics(self):
-        self.phonetics = [x['phonetics'] for x in self.raw_entry[:]]
+        """
+        A list with all the word phonetics and links.
+
+        :return: list with phonetics.
+        :rtype: list
+        """
+        for phonetics in self.raw_entry[:]:
+            if 'phonetics' in phonetics.keys():
+                self.phonetics.append(phonetics['phonetics'])
         return self.phonetics
 
-    def get_all_phonetic_texts(self):
-        self.phonetic_texts = [[phonetic_text['text'] if 'text' in phonetic_text.keys() else None for
-                                phonetic_text in phonetic[:]] for phonetic in self.phonetics[:]]
+    def get_all_phonetic_text_representation(self):
+        """
+        A list with all the phonetic representations the word has.
+
+        :return: list with phonetic text representation.
+        :rtype: list
+        """
+        for phonetics in self.phonetics[:]:
+            for phonetic in phonetics:
+                if 'text' in phonetic.keys() and phonetic['text'] not in self.phonetic_texts:
+                    self.phonetic_texts.append(phonetic['text'])
         return self.phonetic_texts
 
     def get_all_synonyms(self):
-        self.synonyms = [[definitions['synonyms'] for definitions in
-                          parts_of_speech] for parts_of_speech in self.meanings_list[:]]
+        """
+        A list with the synonyms found for the word
+
+        :return: list with synonyms.
+        :rtype: list
+        """
+        for parts_of_speech in self.meanings_list[:]:
+            for part_of_speech in parts_of_speech[:]:
+                if ('definitions' in part_of_speech.keys()) and type(part_of_speech['definitions']) == list:
+                    for definition in part_of_speech['definitions']:
+                        if 'synonyms' in definition.keys() and definition['synonyms'] != []:
+                            self.antonyms.append(definition['synonyms'])
+                if ('antonyms' in part_of_speech.keys()) and part_of_speech['synonyms'] != []:
+                    self.antonyms.append(part_of_speech['synonyms'])
         return self.synonyms
 
     def get_all_antonyms(self):
-        self.antonyms = [[definitions['antonyms'] for definitions in
-                          parts_of_speech] for parts_of_speech in self.meanings_list[:]]
+        """
+        A list with the antonyms found for the word
+
+        :return: list with antonyms.
+        :rtype: list
+        """
+        for parts_of_speech in self.meanings_list[:]:
+            for part_of_speech in parts_of_speech[:]:
+                if ('definitions' in part_of_speech.keys()) and type(part_of_speech['definitions']) == list:
+                    for definition in part_of_speech['definitions']:
+                        if 'antonyms' in definition.keys() and definition['antonyms'] != []:
+                            self.antonyms.append(definition['antonyms'])
+                if ('antonyms' in part_of_speech.keys()) and part_of_speech['antonyms'] != []:
+                    self.antonyms.append(part_of_speech['antonyms'])
         return self.antonyms
 
     def get_all_audio_links(self):
-        self.audio_links = [
-            [phonetics['audio'] if 'audio' in phonetics.keys() else None for phonetics in entry['phonetics']] for
-            entry in self.raw_entry[:]]
+        """
+        A list with the audio pronunciation links
+
+        :return: list with links.
+        :rtype: list
+        """
+        for entry in self.raw_entry[:]:
+            for phonetics in entry['phonetics']:
+                if phonetics['audio'] not in self.audio_links and phonetics['audio'] != '':
+                    self.audio_links.append(phonetics['audio'])
         return self.audio_links
--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-from Dicthon.dictionary_api_entry import Dictionary_API_Entry
+import pprint
 
-
-def create_dict(listOfLists):
-    new_list = []
-    if type(listOfLists) == list:
-        for list_item_first_level in listOfLists:
-            if type(list_item_first_level) == list:
-                for list_item_second_level in list_item_first_level:
-                    if type(list_item_second_level) == list:
-                        for list_item_third_level in list_item_second_level:
-                            if type(list_item_third_level) == list:
-                                print('list too deep, 4 levels')
-                            else:
-                                new_list.append(list_item_third_level)
-                    else:
-                        new_list.append(list_item_second_level)
-            else:
-                new_list.append(list_item_first_level)
-    else:
-        new_list = listOfLists
-
-    return new_list
+from Dicthon import Dictionary_API_Entry
 
 
 if __name__ == '__main__':
-    hello = Dictionary_API_Entry('cock')
-
+    hello = Dictionary_API_Entry('dog')
+    hello.get_dict_entry()
     print(hello.raw_entry)
     print('meanings: ')
-    print(hello.get_all_meanings())
+    pprint.pprint(hello.get_all_meanings())
     print('phonetics: ')
     print(hello.get_all_phonetics())
     print('entry: ')
@@ -38,8 +18,9 @@ if __name__ == '__main__':
     print('parts of speech: ')
     print(hello.get_all_parts_of_speech())
     print('phonetic texts: ')
-    print(create_dict(hello.get_all_phonetic_texts()))
+    print(hello.get_all_phonetic_text_representation())
     print(hello.get_all_synonyms())
+    print(hello.get_all_antonyms())
     print(hello.get_all_definitions_with_example())
-    print(create_dict(hello.get_all_synonyms()))
+    print(hello.get_all_audio_links())
     print(hello.__sizeof__())
